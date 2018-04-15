835ff604aa94ca7f88f470988e694afb1029cddd
hangsunlee <hslee22@sen.go.kr>
2018-04-16 07:57:17 +0900

    4월16일

--- myhome/header.php
+++ myhome/header.php
@@ -3,19 +3,20 @@
 
         <h1 id="logo"><a href="./">서울디지텍고등학교</a></h1>
         <ul class="lnb">
-                <?php
-                if(isset($_SESSION['name'])) {
-                    ?>
-                    <li><a href="./logout.php" ">로그아웃 |</a></li>
-                    <li><a href=""><?=$_SESSION['name']?>님로그인 하였습니다.</a></li>
-                    <?php
-                }else {
-                    ?>
-                    <li><a href="./login.php"id="btn_login">로그인</a>|</li>
-                    <li><a href="./git.php">회원가입</a>|</li>
-                    <?php
-                }
+            <?php
+            if(isset($_SESSION['name'])) {
                 ?>
+                <li><a href="./logout.php" ">로그아웃 |</a></li>
+                <li><a href=""><?=$_SESSION['name']?>님로그인 하였습니다.</a></li>
+                <li><a href="./mypage.php">마이페이지</a></li>
+                <?php
+            }else {
+                ?>
+                <li><a href="./login.php"id="btn_login">로그인</a>|</li>
+                <li><a href="./git.php">회원가입</a>|</li>
+                <?php
+            }
+            ?>
         </ul>
 
     </div>
@@ -24,17 +25,7 @@
             <li>
                 <a href="./portpolio.php">포트폴리오</a>
             </li>
-            <li>자료실
-                <ul class="submenu">
-                    <li><a href="./down/nodefind.zip">노드찾기</a></li>
-                    <li><a href="./down/nodeadd.zip">노드이동추가삭제</a></li>
-                    <li><a href="./down/c4m.zip">ch4미션</a></li>
-                    <li><a href="./down/equals182.eqftp.zip">ftp</a></li>
-                    <li><a href="./down/webtest.zip">기능사문제</a></li>
-                    <li><a href="./down/2gi.zip">2회기능사문제</a></li>
-                    <li><a href="./down/position.zip">JQUERY위치/크기</a></li>
-                    <li><a href="http://api.bus.go.kr/contents/sub02/getStationByUid.html">서울시버스개발가이드</a></li>
-                </ul>
+            <li><a href="./board1.php">자료실</a>
             </li>
             <li>
                 참고사이트
