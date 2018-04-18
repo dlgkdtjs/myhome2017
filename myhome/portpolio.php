12e5b9a9071b47f1aa8106f4e33ec39d131b03e2
hangsunlee <hslee22@sen.go.kr>
2018-04-18 09:09:08 +0900

    포트폴리오 변경 및 사진추가

--- myhome/portpolio.php
+++ myhome/portpolio.php
@@ -7,7 +7,8 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 include_once "./db/db.php";
 $id=$_SESSION['id'];
-$sql="select * from git";
+$grade=$_GET['grade'];
+$sql="select * from git where id LIKE '$grade%'";
 $stmt=$pdo->prepare($sql);
 $stmt->execute();
 $rows=$stmt->fetchAll();
@@ -19,15 +20,30 @@ $rows=$stmt->fetchAll();
     <div id="container">
         <?php
         foreach ($rows as $result) {
-        ?>
+        if($result['lng']==1){
+            $lng="<img src='./img/html5.png' alt='html5css3'>";
+        }else{
+            $lng="없음";
+        }
+
+            ?>
         <ul class="p_page">
             <li class="pro">
-                <img src="" alt="프로필">
+                <p class="my">
+                    <img src="./img/pro.png" width="100" alt="프로필"><br>
+                    <span><?= $result['name'] ?></span>
+                </p>
             </li>
 
-                <li><span><a href="http://<?= $result['home'] ?>" target="_blank"><?=$result['name']?></a></span><span ><a
-                            href="<?= $result['git_url']?> " target="_blank"><img src="./img/github-logo.png" width="100" alt=""></a></span>
+                <li><span><a href="http://<?= $result['home'] ?>" target="_blank"><img src="./img/myhome.png" width='50' title="<?= $result['name'] ?>홈페이지"alt="<?= $result['name'] ?>홈페이지"></a></span><span ><a
+                            href="<?= $result['git_url']?> " target="_blank"><img src="./img/github-logo.png" width="100" title="<?= $result['name'] ?>깃허브"alt="<?= $result['name'] ?>깃허브"></a></span>
                 </li>
+            <li class="lng">
+                사용가능언어
+                <ul>
+                    <li><?=$lng?><span>Lv.<?=$result['level']?></span></li>
+                </ul>
+            </li>
                 <li class="my_info"></li>
         </ul>
             <?php
