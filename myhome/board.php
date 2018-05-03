a83d6fecf1f3c57f728da6e2003f298b6368f080
hangsunlee <hslee22@sen.go.kr>
2018-05-03 10:40:44 +0900

    오류정정

--- myhome/board.php
+++ myhome/board.php
@@ -2,19 +2,25 @@
 <html lang="ko">
 <?php
 session_start();
-if(isset($_SESSION['name'])) {
-    include_once './db/db.php';
-    $ok=$_GET['ok'];
-    $sql = "select * from board WHERE ok='$ok'";
- //   echo $sql;
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $rows = $stmt->fetchAll();
-    include_once "./head.php";
-}else{
+include_once './db/db.php';
+include_once "./head.php";
+if(!isset($_SESSION['name'])) {
     echo "<script>alert('권한이 없습니다.');</script>";
     echo "<script>history.back();</script>";
 }
+if(isset($_GET['ok'])){
+    $ok=$_GET['ok'];
+    $sql = "select * from board WHERE ok='$ok'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll();
+}else{
+    $sql = "select * from board";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll();
+}
+
 ?>
 <body>
 <div id="wrap">
@@ -29,6 +35,8 @@ if(isset($_SESSION['name'])) {
                 <th>제목</th>
                 <th>이름</th>
                 <th>첨부파일</th>
+                <th>수정</th>
+                <th>삭제</th>
             </tr>
             <?php
             foreach ($rows as $result) {
@@ -38,8 +46,12 @@ if(isset($_SESSION['name'])) {
                     <td><?= $result['title'] ?></td>
                     <td><?= $result['name'] ?></td>
                     <td><?php if($result['name']==$_SESSION['name']){ ?>
-                            <span class='down'><a href="<?= $result['file'] ?>">다운로드</a></span>
-                        <?php }else{echo"다운로드";}?></td>
+                    <span class='down'><a href="./down.php?file=<?= $result['file'] ?>">다운로드</a></span>
+                    <?php }else{echo"다운로드";}?></td><td><?php if($result['name']==$_SESSION['name']){ ?>
+                    <span class='down'><a href="./upboard.php?idx=<?=$result['idx']?>">수정</a></span>
+                    <?php }else{echo"수정";}?></td><td><?php if($result['name']==$_SESSION['name']){ ?>
+                    <span class='down'><a href="./delboard.php?idx=<?=$result['idx']?>">삭제</a></span>
+                    <?php }else{echo"삭제";}?></td>
                 </tr>
                 <?php
             }
